82fb499f7af52541315ec04fd56424bb322b0f2a
Zach Champion <zchampion2020@my.fit.edu>
2022-06-21 19:39:42 -0400

    Update to work on raspi

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import axios from "axios";
+import { Gpio } from "onoff";
 
+const btn = new Gpio(17, "in", "rising", { debounceTimeout: 10 });
+
+// Data to send to the plug
 const req_data = {
   turn_off: '{"system":{"set_relay_state":{"state":0}}}',
   turn_on: '{"system":{"set_relay_state":{"state":1}}}',
   get_state: '{"system":{"get_sysinfo":{}}}',
 };
 
+// Send a request to the plug
 function send_request(req_type: string) {
   return axios.post(
     `https://wap.tplinkcloud.com?token=${process.env.TPLINK_TOKEN}`,
@@ -20,10 +25,30 @@ function send_request(req_type: string) {
   );
 }
 
-// Get the current state of the device
-const state_resp = await send_request(req_data.get_state);
-const state_resp_data = JSON.parse(state_resp.data.result.responseData);
-const relay_state = state_resp_data.system.get_sysinfo.relay_state;
+// Toggle the plug
+async function toggle_plug() {
+  // Get the current state of the device
+  const state_resp = await send_request(req_data.get_state);
+  const state_resp_data = JSON.parse(state_resp.data.result.responseData);
+  const relay_state = state_resp_data.system.get_sysinfo.relay_state;
 
-// Toggle the device
-send_request(relay_state ? req_data.turn_off : req_data.turn_on);
+  // Toggle the device
+  return send_request(relay_state ? req_data.turn_off : req_data.turn_on);
+}
+
+// Watch for button presses
+btn.watch((err, value) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Button pressed: ${value}`);
+  toggle_plug();
+});
+
+function cleanup() {
+  btn.unwatch();
+  btn.unexport();
+}
+
+process.on("SIGINT", cleanup);
